b8cb9cca4b33e4f8524bc15cf49bd47272b24309
김민재 <kmj94102@naver.com>
2024-02-03 20:24:09 +0900

    - 상태바 아이콘 색상 적용

--- app/src/main/java/com/example/lolketingcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/lolketingcompose/ui/theme/Theme.kt
@@ -58,7 +58,7 @@ fun LoLketingComposeTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = MyBlack.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+//            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
